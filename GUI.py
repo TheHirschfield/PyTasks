fb879c696a88df61f5b4882b62a46cda61ab38fa
Oliver Hirschfield <oliver@universedevelopment.com>
2016-02-09 17:41:32 +0000

    Added "New" Option To File Menu
    Added the ability to select "New" from the "File" drop down menu and
    select either "Event" or "Tag".

--- GUI.py
+++ GUI.py
@@ -33,11 +33,33 @@ class windowManagement(Frame):
         guiMenuBar = Menu(self.parent)
         self.parent.config(menu=guiMenuBar)
 
-        #File Drop Down Menu
+        #Drop Down Menu -FILE
         guiMenuBarFile = Menu(guiMenuBar)
+
+        #Action Menu - NEW
+        guiMenuBarFileNew = Menu(guiMenuBarFile)
+        
+        #Add Options For NEW Menu
+        guiMenuBarFileNew.add_command(label="Event", command=self.onNewEvent) #New Event
+        guiMenuBarFileNew.add_command(label="Tag", command=self.onNewTag) #New Tag
+        guiMenuBarFile.add_cascade(label="New", menu=guiMenuBarFileNew, underline=0)
+        
+        #Add Separator to Dropdown
+        guiMenuBarFile.add_separator()
+
+        #Quit Option
         guiMenuBarFile.add_command(label="Quit", command=self.onExit)
         guiMenuBar.add_cascade(label="File", menu=guiMenuBarFile)
 
+
+    #New Event
+    def onNewEvent(self):
+        print("This Will Allow The User To Create An Event.")
+
+    #New Tag
+    def onNewTag(self):
+        print("This Will Allow The User To Create A Tag.")
+    
     #Close Window & Quit Program
     def onExit(self):
         self.parent.destroy()
