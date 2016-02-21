523d6bba9f7295cb1068cb3a6fbb2b0ca9eecbab
Oliver Hirschfield <oliver@universedevelopment.com>
2016-02-21 20:23:08 +0000

    Created Notes File

---
+++ Notes.py
@@ -0,0 +1,45 @@
+"""
+                                       
+   Business - U08007                   
+   Calender, Schedule, Planner.        
+                                       
+   Authors: Oliver Hirschfield, Curtis Geddes, Christian Rojas,
+   Francesca Ayeni, Fayosi Olukoya, Kieran St Louis.
+
+   File: Notes Managements
+                                       
+"""
+
+#Import Time Packages
+import csv
+
+#Main Note Storage
+notes = []
+
+def addNote(time, note):
+    #Create Temp New Note List
+    temp = [time, note]
+
+    #Add To Main Storage
+    notes.append(temp)
+
+    #Debug Notify
+    print("Event Added: ", temp)
+
+#Remove Saved Note
+def removeNote(time):
+    
+    for n in range(0, len(notes)):
+        if notes[n][0] == time:
+            del notes[n]
+            print ("Note Removed: ", time)
+
+
+#Get Note For Date
+def getNote(time):
+    for n in range(0, len(notes)):
+        if notes[n][0] == time:
+            return notes[n][1]
+
+    return ""
+        
